ee681d6709015d74d86d3b46e0727eaa0003a937
Oleh <oleh.bihma@rubygarage.org>
2022-09-13 10:37:03 +0300

    HT-4

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@
  * @param {number} maxSecond
  * @returns {number}
  */
-export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
+export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+  let firstResult = 0
+  let secondResult = 0;
+
+  if (minFirst > minSecond) {
+    firstResult = minSecond;
+  } else firstResult = minFirst;
+
+  if (maxFirst > maxSecond) {
+    secondResult = maxFirst;
+  } else secondResult = maxSecond;
+  return firstResult * secondResult;
+};
 
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
@@ -16,7 +28,9 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
  * @param {number} radius
  * @returns {number}
  */
-export const getSquareOfCircle = (radius) => {};
+export const getSquareOfCircle = (radius) => {
+  return Math.PI * (radius ** 2);
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -26,7 +40,9 @@ export const getSquareOfCircle = (radius) => {};
  * @param {number} accuracy
  * @returns {number}
  */
-export const getFixedValue = (number, accuracy) => {};
+export const getFixedValue = (number, accuracy) => {
+  return Math.round(number * +(`1e${accuracy}`)) / +(`1e${accuracy}`);
+};
 
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
@@ -34,7 +50,14 @@ export const getFixedValue = (number, accuracy) => {};
  * @param {number} number
  * @returns {number}
  */
-export const getFlooroCeil = (number) => {};
+export const getFlooroCeil = (number) => {
+  if (Math.trunc(number) % 2 === 0) {
+    return Math.floor(number);
+  }
+  if (Math.trunc(number) % 2 !== 0) {
+    return Math.ceil(number);
+  }
+};
 
 /**
  * Мы передаем два числа, от какого и до какого числа и функция должна
@@ -44,4 +67,6 @@ export const getFlooroCeil = (number) => {};
  * @param {number} to
  * @returns {number}
  */
-export const randomizer = (from, to) => {};
+export const randomizer = (from, to) => {
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+};
